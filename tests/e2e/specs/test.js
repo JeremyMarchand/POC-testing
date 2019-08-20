1686ce5ed1634543b116a7edb794aeb85f69dc7c
JeremyMarchand <jeremy@aono.fr>
2019-08-20 16:47:25 +0200

    Update nightwatch config to handle e2e tests only (TODO : set up .env)

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,14 +1,25 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
+const chromedriver = require('chromedriver');
 
 module.exports = {
-  'default e2e tests': browser => {
+  before: function(browser, done) {
+    chromedriver.start();
+    done();
+  },
+  'default e2e tests': (browser, done) => {
     browser
-      .url(process.env.VUE_DEV_SERVER_URL)
+      .url("http://127.0.0.1:8081")
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.hello')
       .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      //.assert.elementCount('img', 1)
-      .end()
-  }
+      .assert.elementCount('img', 1)
+        .end(done)
+  },
+  after(browser, done) {
+    chromedriver.stop()
+    browser.end(() => {
+      done();
+    });
+  },
 }
